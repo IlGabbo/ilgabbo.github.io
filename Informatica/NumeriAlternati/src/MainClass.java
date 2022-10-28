f96737367840be510243eb89c3e4fddb4186c0cf
zMyster <96393534+IlGabbo@users.noreply.github.com>
2022-10-28 12:56:29 +0200

    Add files via upload

---
+++ Informatica/NumeriAlternati/src/MainClass.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int vettore[] = new int[10];
+
+        System.out.println("Originale");
+        for (int i = 0; i < vettore.length; i++) {
+            int random = rand.nextInt(101 / 2) * 2 + 1;
+            vettore[i] = random;
+            System.out.println(vettore[i]);
+        }
+
+        System.out.println("Alternati");
+        for (int i = 0; i < vettore.length - 1; i++) {
+            try {
+                int alternanza = vettore[i] + vettore[i + 1];
+                vettore[i + 1] = alternanza;
+                System.out.println(vettore[i]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
