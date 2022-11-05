1c82d3764ca7779d4ca628126f13b6fe42ba7250
zMyster <96393534+IlGabbo@users.noreply.github.com>
2022-11-05 09:14:01 +0100

    Delete geolocation directory

--- geolocation/js.js
+++
@@ -1,6 +0,0 @@
-
-navigator.geolocation.getCurrentPosition(showPosition);
-
-function showPosition(position) {
-   document.write("Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude);
-}
