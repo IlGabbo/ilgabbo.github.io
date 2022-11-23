d19f4397eba88410a1890e70441634e155130035
IlGabbo <f.mancini.gabriele@gmail.com>
2022-11-23 21:01:00 +0100

    Media Query, l'indice delle notifiche si azzera quando premuto

--- static/js/js.js
+++ static/js/js.js
@@ -1,6 +1,21 @@
 let giglo = document.querySelector(".about")
 let window_notification = document.getElementById("window-notification")
 let isWindowNotificationOpen = false
+let screenSize = screen.width
+let indexNotification = document.querySelector(".notification")
+
+
+indexNotification.setAttribute("data", "1")
+
+indexNotification.addEventListener("click", function(e) {
+        indexNotification.setAttribute("data", "0")
+    })
+
+function showMenu() {
+    let e = window.event
+    e.preventDefault()
+    document.querySelector(".options").classList.toggle("show-options")
+}
 
 function showAbout() {
     giglo.classList.toggle("about-me")
@@ -9,17 +24,14 @@ function showAbout() {
 function notification() {
     let e = window.event
     e.preventDefault()
-    if (isWindowNotificationOpen == false) {
+    if (screenSize > 480 && screenSize > 768) {
         const {clientX: X, clientY: Y} = e
-        window_notification.style.display = "flex"
         window_notification.style.position = "fixed"
         window_notification.style.top = Y + 10 + "px"
         window_notification.style.left = X + "px"
-        isWindowNotificationOpen = true
-    } else if (isWindowNotificationOpen == true) {
-        window_notification.style.display = "None"
-        isWindowNotificationOpen = false
+        window_notification.classList.toggle("showHide-window-notification")    
+    } else {
+        window_notification.classList.toggle("centered-window-notification")    
     }
-    
 }
 
