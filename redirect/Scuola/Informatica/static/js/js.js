6503ad7aa54037a23092d29215de7d368e7b3999
IlGabbo <f.mancini.gabriele@gmail.com>
2022-11-24 16:34:34 +0100

    Design interamente rivisto, da aggiungere le query

--- redirect/Scuola/Informatica/static/js/js.js
+++ redirect/Scuola/Informatica/static/js/js.js
@@ -1,5 +1,41 @@
-let giglo = document.querySelector(".about")
+let image = document.getElementById("image")
+let menu = document.getElementById("context-menu")
+let content = document.getElementById("Lorem")
+let notify = document.getElementById("notifications")
+let notificationBox = document.getElementById("message-box")
+let title = document.getElementById("title")
+let isBoxOpened = false
 
-function showAbout() {
-    giglo.classList.toggle("about-me")
-}
+
+
+notify.setAttribute("index", "1")
+notify.addEventListener("click", function(e) {
+    e.preventDefault()
+    if (isBoxOpened == false) {
+        const {clientX: X, clientY: Y} = e
+        notify.setAttribute("index", "0")
+        notificationBox.style.display = "flex"
+        notificationBox.style.position = "fixed"
+        notificationBox.style.top = Y + 15 + "px"
+        notificationBox.style.left = X + "px"
+        isBoxOpened = true
+    } else {
+        notificationBox.style.display = "none"
+        isBoxOpened = false
+    }
+
+})
+
+window.addEventListener("scroll", 
+    function(e) {
+        if (this.scrollY > 20) {
+            //menu.classList.add("shadow-in")
+            menu.style.backgroundColor = "#000"
+            title.style.color = "#fff"
+        } else {
+            //menu.classList.remove("shadow-in")
+            menu.style.backgroundColor = "transparent"
+            title.style.color = "#AFAFAF"
+        }
+        menu.style.transition = "all 0.2s"
+    })
--- static/js/js.js
+++ static/js/js.js
@@ -1,37 +1,62 @@
-let giglo = document.querySelector(".about")
-let window_notification = document.getElementById("window-notification")
-let isWindowNotificationOpen = false
-let screenSize = screen.width
-let indexNotification = document.querySelector(".notification")
+let image = document.getElementById("image")
+let menu = document.getElementById("context-menu")
+let content = document.getElementById("Lorem")
+let notify = document.getElementById("notifications")
+let notificationBox = document.getElementById("message-box")
+let title = document.getElementById("title")
+let isBoxOpened = false
 
 
-indexNotification.setAttribute("data", "1")
 
-indexNotification.addEventListener("click", function(e) {
-        indexNotification.setAttribute("data", "0")
+notify.setAttribute("index", "1")
+notify.addEventListener("click", function(e) {
+    e.preventDefault()
+    if (isBoxOpened == false) {
+        const {clientX: X, clientY: Y} = e
+        notify.setAttribute("index", "0")
+        notificationBox.style.display = "flex"
+        notificationBox.style.position = "fixed"
+        notificationBox.style.top = Y + 15 + "px"
+        notificationBox.style.left = X + "px"
+        isBoxOpened = true
+    } else {
+        notificationBox.style.display = "none"
+        isBoxOpened = false
+    }
+
+})
+
+window.addEventListener("scroll", 
+    function(e) {
+        if (this.scrollY > 20) {
+            //menu.classList.add("shadow-in")
+            menu.style.backgroundColor = "#000"
+            title.style.color = "#fff"
+        } else {
+            //menu.classList.remove("shadow-in")
+            menu.style.backgroundColor = "transparent"
+            title.style.color = "#AFAFAF"
+        }
+        menu.style.transition = "all 0.2s"
     })
 
-function showMenu() {
-    let e = window.event
-    e.preventDefault()
-    document.querySelector(".options").classList.toggle("show-options")
-}
+document.querySelector("#image").addEventListener("mousemove", 
+    function(e) {
+        hasAlreadyVisited = true
+        title.style.top = "5px"
+        image.style.width = "4%"
+        image.style.height = "8vh"
+        image.style.borderRadius = "100px"
+        image.style.border = "2px solid #fff"
+        image.style.backgroundPosition = "center"
+        image.style.top = "10px"
+        image.style.left = "20px"
+        menu.style.left = "0px"
+        content.style.left = "25%"
+        document.querySelector("body").style.overflow = "visible"
+        image.style.transition = "all 1.5s"
+        menu.style.transition = "all 1.5s"
+        content.style.transition = "all 2.3s"
+        title.style.transition = "all 1.5s"
 
-function showAbout() {
-    giglo.classList.toggle("about-me")
-}
-
-function notification() {
-    let e = window.event
-    e.preventDefault()
-    if (screenSize > 480 && screenSize > 768) {
-        const {clientX: X, clientY: Y} = e
-        window_notification.style.position = "fixed"
-        window_notification.style.top = Y + 10 + "px"
-        window_notification.style.left = X + "px"
-        window_notification.classList.toggle("showHide-window-notification")    
-    } else {
-        window_notification.classList.toggle("centered-window-notification")    
-    }
-}
-
+    })
