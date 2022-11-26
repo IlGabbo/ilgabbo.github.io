89a3c3a465c88c7c9c0425b51db2dfa9b21c2bc5
IlGabbo <f.mancini.gabriele@gmail.com>
2022-11-26 08:52:58 +0100

    Media Query a tutto

--- redirect/Scuola/static/js/js.js
+++ redirect/Scuola/static/js/js.js
@@ -4,38 +4,74 @@ let content = document.getElementById("Lorem")
 let notify = document.getElementById("notifications")
 let notificationBox = document.getElementById("message-box")
 let title = document.getElementById("title")
+let content_box = document.getElementById("content-box")
 let isBoxOpened = false
 
 
 
-notify.setAttribute("index", "1")
+notify.setAttribute("index", "0")
 notify.addEventListener("click", function(e) {
     e.preventDefault()
     if (isBoxOpened == false) {
-        const {clientX: X, clientY: Y} = e
         notify.setAttribute("index", "0")
         notificationBox.style.display = "flex"
         notificationBox.style.position = "fixed"
-        notificationBox.style.top = Y + 15 + "px"
-        notificationBox.style.left = X + "px"
+        if (window.innerWidth > 1300) {
+            console.log(window.innerWidth)
+            const {clientX: X, clientY: Y} = e
+            notificationBox.style.top = Y + 15 + "px"
+            notificationBox.style.left = X + "px"
+        } else {
+            console.log(window.innerWidth)
+            notificationBox.style.bottom = "0"
+            notificationBox.style.right = "0"
+        }
         isBoxOpened = true
     } else {
         notificationBox.style.display = "none"
         isBoxOpened = false
     }
-
 })
 
-window.addEventListener("scroll", 
+window.addEventListener("scroll",
     function(e) {
+        content_box.style.top = "70%"
+        content_box.style.transition = "all 0.8s"
         if (this.scrollY > 20) {
             //menu.classList.add("shadow-in")
             menu.style.backgroundColor = "#000"
             title.style.color = "#fff"
+            
         } else {
             //menu.classList.remove("shadow-in")
             menu.style.backgroundColor = "transparent"
             title.style.color = "#AFAFAF"
+
         }
         menu.style.transition = "all 0.2s"
     })
+
+document.querySelector(".hamburger a").addEventListener("click", function(e){
+    e.preventDefault()
+    document.querySelector(".options").classList.toggle("toggle-menu")
+})
+
+function showAboutMe() {
+    let about_window = document.getElementById("about")
+    if (window.screenX > 1300) {
+        let e = window.event
+        const {clientX: X, clientY: Y} = e
+        e.preventDefault()
+        about_window.classList.toggle("show-about-me")
+        about_window.style.top = `${Y + 15}px`
+        about_window.style.left = `${X}px`
+    } else {
+        about_window.classList.toggle("query-about")
+    }
+}
+    
+
+function installerRedirect() {
+    window.open("https://github.com/darksnakepy/Design-QT", "_self")
+}
+
--- static/js/js.js
+++ static/js/js.js
@@ -4,18 +4,19 @@ let content = document.getElementById("Lorem")
 let notify = document.getElementById("notifications")
 let notificationBox = document.getElementById("message-box")
 let title = document.getElementById("title")
+let content_box = document.getElementById("content-box")
 let isBoxOpened = false
 
 
 
-notify.setAttribute("index", "1")
+notify.setAttribute("index", "0")
 notify.addEventListener("click", function(e) {
     e.preventDefault()
     if (isBoxOpened == false) {
         notify.setAttribute("index", "0")
         notificationBox.style.display = "flex"
         notificationBox.style.position = "fixed"
-        if (window.innerWidth > 1200) {
+        if (window.innerWidth > 1300) {
             console.log(window.innerWidth)
             const {clientX: X, clientY: Y} = e
             notificationBox.style.top = Y + 15 + "px"
@@ -30,63 +31,67 @@ notify.addEventListener("click", function(e) {
         notificationBox.style.display = "none"
         isBoxOpened = false
     }
-
 })
 
-window.addEventListener("scroll", 
+window.addEventListener("scroll",
     function(e) {
+        content_box.style.top = "70%"
+        content_box.style.transition = "all 0.8s"
         if (this.scrollY > 20) {
             //menu.classList.add("shadow-in")
             menu.style.backgroundColor = "#000"
             title.style.color = "#fff"
+            
         } else {
             //menu.classList.remove("shadow-in")
             menu.style.backgroundColor = "transparent"
             title.style.color = "#AFAFAF"
+
         }
         menu.style.transition = "all 0.2s"
     })
 
-if (window.innerWidth > 1200) {
-    document.querySelector("#image").addEventListener("mousemove", 
-    function(e) {
-        title.style.top = "5px"
-        image.style.width = "4%"
-        image.style.height = "8vh"
-        image.style.borderRadius = "100px"
-        image.style.border = "2px solid #fff"
-        image.style.backgroundPosition = "center"
-        image.style.top = "10px"
-        image.style.left = "20px"
-        menu.style.left = "0px"
-        content.style.left = "25%"
-        document.querySelector("body").style.overflow = "visible"
-        image.style.transition = "all 1.5s"
-        menu.style.transition = "all 1.5s"
-        content.style.transition = "all 2.3s"
-        title.style.transition = "all 1.5s"
-
-    })
-} else {
-    title.style.top = "25px"
-    image.style.width = "4%"
-    image.style.height = "8vh"
+document.querySelector("#image").addEventListener("mousemove", 
+function(e) {
+    title.style.top = "0.8vh"
+    image.style.width = "60px"
+    image.style.height = "60px"
     image.style.borderRadius = "100px"
-    image.style.border = "2px solid #fff"
+    image.style.border = "1px solid #fff"
     image.style.backgroundPosition = "center"
     image.style.top = "10px"
     image.style.left = "20px"
     menu.style.left = "0px"
     content.style.left = "25%"
-    document.querySelector("body").style.overflow = "visible"
+    document.querySelector("body").style.overflowY = "visible"
     image.style.transition = "all 1.5s"
     menu.style.transition = "all 1.5s"
     content.style.transition = "all 2.3s"
     title.style.transition = "all 1.5s"
-}
+
+})
 
 document.querySelector(".hamburger a").addEventListener("click", function(e){
     e.preventDefault()
     document.querySelector(".options").classList.toggle("toggle-menu")
 })
+
+function showAboutMe() {
+    let about_window = document.getElementById("about")
+    if (window.screenX > 1300) {
+        let e = window.event
+        const {clientX: X, clientY: Y} = e
+        e.preventDefault()
+        about_window.classList.toggle("show-about-me")
+        about_window.style.top = `${Y + 15}px`
+        about_window.style.left = `${X}px`
+    } else {
+        about_window.classList.toggle("query-about")
+    }
+}
+    
+
+function installerRedirect() {
+    window.open("https://github.com/darksnakepy/Design-QT", "_self")
+}
 
