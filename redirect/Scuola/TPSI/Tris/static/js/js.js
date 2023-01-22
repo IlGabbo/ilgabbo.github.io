1b71571b9c6e0f3b1be5a7af836757af67d7ef10
IlGabbo <f.mancini.gabriele@gmail.com>
2023-01-22 21:53:55 +0100

    Tried adding a bot

--- redirect/Scuola/TPSI/Tris/static/js/js.js
+++ redirect/Scuola/TPSI/Tris/static/js/js.js
@@ -1,8 +1,16 @@
 let music = new Audio("static/music/music.ogg")
-let volume_slide = document.getElementById("volume")
+let pop = new Audio("static/music/pop.ogg")
+let win = new Audio("static/music/levelup.ogg")
+let general = 0.2
+let music_volume_slide = document.getElementById("music-volume")
+let general_volume = document.getElementById("general-volume")
 let volume_icon = document.getElementById("volume-icon")
 let th = document.querySelectorAll("th")
+let thSp = document.querySelectorAll(".table tr th")
 let tr = document.querySelectorAll("tr")
+let difficulty = "easy"
+let inc = 0
+let incRow = 0
 let value = null
 let cells = [
     [null, null, null],  // 0
@@ -12,19 +20,33 @@ let cells = [
 let count = 0
 let volume_mute = true         
 let is_x_written = false                     
-music.loop = true                
+music.loop = true               
 
 function loadMusic() {
     music.play()
 }
 
-function volume(value) {
-    music.volume = parseInt(value/100)
-    document.getElementById("volume-status").innerHTML = `Volume: ${value}`
-}
-
 function showHideSettings() {
     document.getElementById("settings-window").classList.toggle("show-hide")
+}
+
+function openMultiplayer() {
+    document.getElementById('game').style.display = 'flex'
+    let click = new Audio("static/music/click.ogg")
+    click.volume = general
+    click.play()
+    multiplayer()
+}
+
+function openSp() {
+    document.getElementById('sp').style.display = 'flex'
+    let click = new Audio("static/music/click.ogg")
+    click.volume = general
+    click.play()
+    document.querySelector(".close-window").addEventListener("click", (e) => {
+        document.querySelector(".difficulty").style.display = "none"
+        playWithBot(difficulty)
+    })
 }
 
 function checkVictory(cells) {
@@ -48,25 +70,87 @@ function checkVictory(cells) {
     }
 }
 
-function resetGame() {
-    let reset_audio = new Audio("static/music/anvil_land.ogg")
-    reset_audio.volume = 0.3
-    reset_audio.play()
-    th.forEach(th => th.innerHTML = "")
-    cells = [
-        [null, null, null],  // 0
-        [null, null, null],  // 1
-        [null, null, null]   // 2
+function resetGame(src) {
+    if (src != null) {
+        let reset_audio = new Audio(src)
+        reset_audio.volume = general
+        reset_audio.play()
+        th.forEach(th => th.innerHTML = "")
+        cells = [
+            [null, null, null],  // 0
+            [null, null, null],  // 1
+            [null, null, null]   // 2
+        ]
+        is_x_written = false
+        value = null
+        count = 0
+    }
+}
+
+function playWithBot(difficulty) {    
+    console.log(difficulty)
+    let available_cells = [
+        [1,2,3],
+        [1,2,3],
+        [1,2,3]
     ]
-    is_x_written = false
-    value = null
-    count = 0
+    switch(difficulty) {
+        case "easy":
+            thSp.forEach(th => th.addEventListener("click", () => {
+                win.volume = general
+                if (th.textContent == "") {
+                    /* value = "X"
+                    th.innerHTML = value */
+                    cells[th.parentNode.getAttribute("row")][th.getAttribute("cell")] = value
+                    available_cells[th.parentNode.getAttribute("row")][th.getAttribute("cell")] = null
+                    pop.src = "static/music/pop.ogg"
+                    let row = Math.floor(Math.random()*available_cells.length)
+                    let cell = Math.floor(Math.random()*row)
+
+                    do {
+                        row = Math.floor(Math.random()*available_cells.length)
+                        cell = Math.floor(Math.random()*row)
+                    } while (cells[row][cell] != null && available_cells[row][cell] != null)
+                    available_cells[row][cell] = null
+                    cells[row][cell] = "O"
+                    console.log(cells)
+                    console.log(available_cells)
+                    console.log(th)
+
+                } else {
+                    pop.src = "static/music/break.ogg"           
+                }
+                read()
+                pop.volume = general
+                pop.play()
+            }))
+            break
+    }
+}
+
+function read() {
+    thSp.forEach(th => {
+        console.log(cells[incRow][inc])
+        th.innerHTML = cells[incRow][inc]
+        inc++
+        if (inc >= 3) {
+            inc = 0
+            incRow += 1
+        }
+    })
 }
 
 
-volume_slide.addEventListener("input", (e) => {
+
+
+music_volume_slide.addEventListener("input", (e) => {
     music.volume = e.currentTarget.value/100
-    document.getElementById("volume-status").innerHTML = `Volume: ${e.currentTarget.value}`
+    document.getElementById("volume-status").innerHTML = `Music: ${e.currentTarget.value}%`
+})
+
+general_volume.addEventListener("input", (e) => {
+    general = e.currentTarget.value/100
+    document.getElementById("general-volume-status").innerHTML = `General Volume: ${e.currentTarget.value}%`
 })
 
 
@@ -82,47 +166,71 @@ volume_icon.addEventListener("click", () => {
     }
 })
 
-th.forEach(th => th.addEventListener("click", () => {
-    let pop = new Audio("static/music/pop.ogg")
-    let win = new Audio("static/music/levelup.ogg")
-    win.volume = 0.5
-    if (th.textContent == "") {
-        if (!is_x_written) {
-            value = "X"
-            th.innerHTML = value            
-            is_x_written = true
+function multiplayer() {
+    th.forEach(th => th.addEventListener("click", () => {
+        win.volume = general
+        if (th.textContent == "") {
+            pop.src = "static/music/pop.ogg"
+            if (!is_x_written) {
+                value = "X"
+                th.innerHTML = value            
+                is_x_written = true
+            } else {
+                value = "O"
+                th.innerHTML = value
+                is_x_written = false
+            }
+            count++
         } else {
-            value = "O"
-            th.innerHTML = value
-            is_x_written = false
+            pop.src = "static/music/break.ogg"
         }
-    } else {
-        pop.src = "static/music/break.ogg"
-    }
+    
+        cells[th.parentNode.getAttribute("row")][th.getAttribute("cell")] = value
+    
+        if (checkVictory(cells) == "X") {
+            document.querySelector("#status").style.display = "flex"
+            document.querySelector(".winner").innerHTML = "'X' won"
+            win.play()
+        } else if (checkVictory(cells) == "O") {
+            document.querySelector("#status").style.display = "flex"
+            document.querySelector(".winner").innerHTML = "'O' won"
+            win.play()
+        } else if (count == 9) {
+            document.querySelector("#status").style.display = "flex"
+            document.querySelector(".winner").innerHTML = "Drawing"
+            pop.src = "static/music/break.ogg"
+        }
+        pop.volume = general
+        pop.play()    
+    }))
+}
 
-    cells[th.parentNode.getAttribute("row")][th.getAttribute("cell")] = value
-    count++
-    console.log(checkVictory(cells))
-    if (checkVictory(cells) == "X") {
-        document.querySelector("#status").style.display = "flex"
-        document.querySelector(".winner").innerHTML = "'X' won"
-        win.play()
-    } else if (checkVictory(cells) == "O") {
-        document.querySelector("#status").style.display = "flex"
-        document.querySelector(".winner").innerHTML = "'O' won"
-        win.play()
-    } else if (count == 9) {
-        document.querySelector("#status").style.display = "flex"
-        document.querySelector(".winner").innerHTML = "Drawing"
-        pop.src = "static/music/break.ogg"
-    }
-    pop.volume = 0.5
-    pop.play()    
-}))
 
-document.getElementById("reset").addEventListener("click", resetGame)
+document.getElementById("reset").addEventListener("click", () => resetGame("static/music/anvil_land.ogg"))
+
 document.getElementById("home").addEventListener("click", () => {
-    resetGame()
+    resetGame("static/music/click.ogg")
     document.getElementById("game").style.display = "none"
 })
+document.querySelector(".difficulty-selector").addEventListener("click", (e) => {
+    let button = e.currentTarget
+    switch(button.textContent) {
+        case "Easy":
+            button.innerHTML = "Medium"
+            button.style.backgroundImage = "url(static/images/background/medium_button.png)"
+            difficulty = "medium"
+            break
+        case "Medium":
+            button.innerHTML = "Hard"
+            button.style.backgroundImage = "url(static/images/background/hard_button.png)"
+            difficulty = "hard"
+            break
+        case "Hard":
+            button.innerHTML = "Easy"
+            button.style.backgroundImage = "url(static/images/background/easy_button.png)"
+            difficulty = "easy"
+            break  
+    }
+})
+
 
